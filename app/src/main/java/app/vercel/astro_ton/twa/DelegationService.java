7aa23278c9efff5b6454ae18fbd8d109cfc13481
William Chen <wichenpps@gmail.com>
2025-08-29 21:41:19 +0200

    feat: android app

---
+++ app/src/main/java/app/vercel/astro_ton/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.vercel.astro_ton.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
