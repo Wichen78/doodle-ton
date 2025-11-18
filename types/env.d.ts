152f5d6dfbf87768d496e7c2adad8b72bd8cc692
William Chen <wichenpps@gmail.com>
2025-11-18 16:03:54 +0100

    fix: request Telegram authorization from the user

--- types/env.d.ts
+++ types/env.d.ts
@@ -5,7 +5,7 @@ declare global {
 		interface ProcessEnv {
 			BOT_TOKEN: string;
 			USER_TEST: string;
-			NEXT_PUBLIC_TG_LOGIN_BOT_TOKEN: number;
+			NEXT_PUBLIC_TG_LOGIN_BOT_TOKEN: string;
 			NEXT_PUBLIC_ALLOW_ALL_DEVICES: boolean;
 			NEXT_PUBLIC_BYPASS_TELEGRAM_AUTH: boolean;
 			ENV: 'test' | 'dev' | 'prod';
